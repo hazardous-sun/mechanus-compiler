b6404a4002bf332558df7ecc0bffa0d993d3400c
solaire <141582502+hazardous-sun@users.noreply.github.com>
2025-03-28 16:47:23 -0300

    started to move the logic to parser.go

--- src/cmd/main.go
+++ src/cmd/main.go
@@ -1,78 +1,46 @@
 package main
 
 import (
-	"errors"
 	"mechanus-compiler/src/error"
 	"mechanus-compiler/src/models"
 	"os"
 )
 
-// TODO check for Active and Inative values for State variables
-// TODO check why Send function is not being correctly shown
-// TODO check why the Construct name is not being collected
-
 func main() {
-	// Initialize input and output files insde Lexical
-	lex := models.NewLexical(nil, nil)
-	inputFile, err := os.Open("examples/example1.mecha")
+	// Open source file
+	sourceFile, err := os.Open("examples/example1.mecha")
 
 	if err != nil {
 		custom_errors.Log(custom_errors.FileOpenError, &err, custom_errors.ErrorLevel)
 		return
 	}
-	lex.InputFile = inputFile
-	defer lex.Close("input")
+	defer func(inputFile *os.File) {
+		err := inputFile.Close()
+		if err != nil {
+			custom_errors.Log(custom_errors.FileCloseError, &err, custom_errors.ErrorLevel)
+		}
+	}(sourceFile)
 
+	// Create output file
 	outputFile, err := os.Create("output.txt")
 
 	if err != nil {
 		custom_errors.Log(custom_errors.FileCreateError, &err, custom_errors.ErrorLevel)
 		return
 	}
-	lex.OutputFile = outputFile
-	defer lex.Close("output")
-
-	// Start looking for tokens
-	err = lex.ReadLines()
-
-	if err != nil {
-		custom_errors.Log(custom_errors.EmptyFile, &err, custom_errors.ErrorLevel)
-		return
-	}
-
-	err = lex.MoveLookAhead()
-
-	if err != nil {
-		return
-	}
-
-	for lex.Token != models.TInputEnd && lex.Token != models.TLexError {
-		err = lex.NextToken()
-
-		if !comment(&lex) {
-			lex.DisplayToken()
-		}
-
+	defer func(outputFile *os.File) {
+		err := outputFile.Close()
 		if err != nil {
-			break
+			custom_errors.Log(custom_errors.FileCloseError, &err, custom_errors.ErrorLevel)
 		}
+	}(outputFile)
+
+	// Initialize the parser
+	parser := models.Parser{
+		Source: sourceFile,
+		Output: outputFile,
 	}
 
-	if lex.Token == models.TLexError {
-		err = errors.New(lex.ErrorMessage)
-		custom_errors.Log(custom_errors.LexicalError, &err, custom_errors.ErrorLevel)
-	} else {
-		custom_errors.Log(custom_errors.LexicalSuccess, nil, custom_errors.SuccessLevel)
-		err = lex.WriteOutput()
-	}
-
-	//lex.ShowTokens()
-	return
-}
-
-func comment(lex *models.Lexical) bool {
-	return lex.Token == models.TSingleLineComment ||
-		lex.Token == models.TOpenMultilineComment ||
-		lex.Token == models.TCloseMultilineComment ||
-		lex.CommentBlock == true
+	// Start the syntax analysis
+	parser.Run()
 }
--- src/models/parser.go
+++ src/models/parser.go
@@ -1,6 +1,64 @@
 package models
 
+import (
+	"errors"
+	custom_errors "mechanus-compiler/src/error"
+	"os"
+)
+
 // Parser :
-// This is the structure responsible for analyzing the syntax of the source file. It interacts directly with the lexical
+// This is the structure responsible for analyzing the syntax of the Source file. It interacts directly with the lexical
 // analyzer and checks recursively for invalid tokens.
-type Parser struct{}
+type Parser struct {
+	Source *os.File
+	Output *os.File
+}
+
+func (p *Parser) Run() {
+	// Initialize input and Output files insde Lexical
+	lex := NewLexical(p.Source, p.Output)
+
+	// Start looking for tokens
+	err := lex.ReadLines()
+
+	if err != nil {
+		custom_errors.Log(custom_errors.EmptyFile, &err, custom_errors.ErrorLevel)
+		return
+	}
+
+	err = lex.MoveLookAhead()
+
+	if err != nil {
+		return
+	}
+
+	for lex.Token != TInputEnd && lex.Token != TLexError {
+		err = lex.NextToken()
+
+		if !comment(&lex) {
+			lex.DisplayToken()
+		}
+
+		if err != nil {
+			break
+		}
+	}
+
+	if lex.Token == TLexError {
+		err = errors.New(lex.ErrorMessage)
+		custom_errors.Log(custom_errors.LexicalError, &err, custom_errors.ErrorLevel)
+	} else {
+		custom_errors.Log(custom_errors.LexicalSuccess, nil, custom_errors.SuccessLevel)
+		err = lex.WriteOutput()
+	}
+
+	//lex.ShowTokens()
+	return
+}
+
+func comment(lex *Lexical) bool {
+	return lex.Token == TSingleLineComment ||
+		lex.Token == TOpenMultilineComment ||
+		lex.Token == TCloseMultilineComment ||
+		lex.CommentBlock == true
+}
