c6bcc57b2b590f809ab769dff079a218abf02269
solaire <141582502+hazardous-sun@users.noreply.github.com>
2025-03-28 16:47:23 -0300

    added a new level 'success' and changed the color of the InfoLevel messages to cyan

--- error/log.go
+++ error/log.go
@@ -9,22 +9,27 @@ const (
 	InfoLevel    = "info"
 	WarningLevel = "warning"
 	ErrorLevel   = "error"
+	SuccessLevel = "success"
 )
 
 const (
 	defaultColor = "\033[0m"
+	infoColor    = "\033[36m"
 	errorColor   = "\033[91m"
 	warningColor = "\033[93m"
+	successColor = "\033[32m"
 )
 
 func Log(message string, err *error, level string) {
 	switch level {
 	case InfoLevel:
-		log.Println(fmt.Sprintf("%sinfo: %s %s", defaultColor, message, defaultColor))
+		log.Println(fmt.Sprintf("%sinfo: %s %s", infoColor, message, defaultColor))
 	case WarningLevel:
 		log.Println(fmt.Sprintf("%swarning: %s -> %v %s", warningColor, message, *err, defaultColor))
 	case ErrorLevel:
 		log.Println(fmt.Sprintf("%serror: %s -> %v %s", errorColor, message, *err, defaultColor))
+	case SuccessLevel:
+		log.Println(fmt.Sprintf("%ssuccess: %s %s", successColor, message, defaultColor))
 	default:
 		log.Println("invalid log level")
 	}
