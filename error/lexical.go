0ebff983f19f46730c920e7b7646dbfe52616302
solaire <141582502+hazardous-sun@users.noreply.github.com>
2025-03-28 16:47:23 -0300

    refactored the logging for lex.ShowTokens

--- error/lexical.go
+++ error/lexical.go
@@ -1,6 +1,7 @@
 package custom_errors
 
 const (
-	LexicalSuccess = "lexical analysis completed with no errors"
-	LexicalError   = "lexical analysis completed with an error"
+	LexicalSuccess   = "lexical analysis completed with no errors"
+	LexicalError     = "lexical analysis completed with an error"
+	IdentifiedTokens = "Identified Tokens (Token/Lexeme):"
 )
