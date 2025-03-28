fc0da005425d586844fc793eabb939d69fc2d4ff
solaire <141582502+hazardous-sun@users.noreply.github.com>
2025-03-28 16:47:23 -0300

    started to implement the syntax analyzer

---
+++ src/models/parser.go
@@ -0,0 +1,6 @@
+package models
+
+// Parser :
+// This is the structure responsible for analyzing the syntax of the source file. It interacts directly with the lexical
+// analyzer and checks recursively for invalid tokens.
+type Parser struct{}
